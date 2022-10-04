52ad2c909700bae1c4c474d419c439d2879bbbba
vtuguide <114895411+vtuguide@users.noreply.github.com>
2022-10-04 19:26:36 +0530

    Create quote.js

---
+++ JS/quote.js
@@ -0,0 +1,17 @@
+const api = "https://api.quotable.io/random";
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const btn = document.getElementById("btn");
+
+
+function getQuote() {
+  fetch(api)
+    .then((res) => res.json())
+    .then((data) => {
+      quote.innerHTML = `"${data.content}"`;
+      author.innerHTML = `- ${data.author}`;
+    });
+}
+
+window.onload = getQuote;
